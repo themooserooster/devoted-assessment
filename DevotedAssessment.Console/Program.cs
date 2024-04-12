662b75a2da99ca617c5728629ec105eaaf012359
Steve Price <steveprice@mooserooster.com>
2024-04-12 13:09:41 -0500

    convert to OG console app style

--- DevotedAssessment.Console/Program.cs
+++ DevotedAssessment.Console/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var running = true;
+
+        while (running)
+        {
+
+            var echo = Console.ReadLine();
+
+            if (echo.Equals("exit", StringComparison.InvariantCultureIgnoreCase)) break;
+
+            Console.WriteLine(echo);
+
+        }
+    }
+}
